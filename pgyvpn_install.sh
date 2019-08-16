55e23fff62c850fe4b96002d3e6dca00b40c23d1
a512154224 <512154224@qq.com>
2019-08-16 17:23:07 +0800

    Update pgyvpn_install.sh

--- pgyvpn_install.sh
+++ pgyvpn_install.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
-wget https://raw.githubusercontent.com/a512154224/share/master/pgyvpn.tar.gz /root
-mkdir /root/pgyvpninstall>/dev/null 2>&1 &
-tar zxf pgyvpn.tar.gz -C /root/pgyvpninstall/
-cp /root/pgyvpninstall/pgyvpn/usr/share/pgyvpn/ /usr/share/ -R
-cp /root/pgyvpninstall/pgyvpn/usr/sbin/pgyvpn* /usr/sbin/
-cp /root/pgyvpninstall/pgyvpn/.pgyvpnlib/ /root -R
-rm -rf /root/pgyvpn*
-echo "pgyvpn install success"
+
+case $1 in
+	install)
+		wget https://raw.githubusercontent.com/a512154224/share/master/pgyvpn.tar.gz /root
+		mkdir /root/pgyvpninstall>/dev/null 2>&1
+		mkdir /etc/pgyvpn>/dev/null 2>&1
+		tar zxf pgyvpn.tar.gz -C /root/pgyvpninstall/
+		cp /root/pgyvpninstall/pgyvpn/usr/share/pgyvpn/ /usr/share/ -R
+		cp /root/pgyvpninstall/pgyvpn/usr/sbin/pgyvpn* /usr/sbin/
+		cp /root/pgyvpninstall/pgyvpn/.pgyvpnlib/ /root -R
+		mv /root/pgyvpninstall/pgyvpn/pgyvpn /etc/pgyvpn
+		rm -rf /root/pgyvpninstall&&rm -rf /root/pgyvp
+		echo "pgyvpn install success"
+		/usr/share/pgyvpn/script/pgyvpn_monitor >/dev/null 2>&1 &
+		;;
+	uninstall)
+		killall pgyvpn_svr 2>/dev/null
+		killall pgyvpn_oraysl 2>/dev/null
+		rm /usr/sbin/pgyvpn*
+		rm /root/.pgyvpnlib/ -r
+		rm /usr/share/pgyvpn/ -r
+		rm /etc/pgyvpn -r
+		;;
+	*)
+		echo "pgyvpn uninstall success"
+		;;
+esac
