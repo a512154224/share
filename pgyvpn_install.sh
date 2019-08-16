eefba6561957acc273b63a62bb3dd5ae2e954f17
a512154224 <512154224@qq.com>
2019-08-16 17:10:46 +0800

    Update pgyvpn_install.sh

--- pgyvpn_install.sh
+++ pgyvpn_install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+wget https://raw.githubusercontent.com/a512154224/share/master/pgyvpn.tar.gz /root
 mkdir /root/pgyvpninstall>/dev/null 2>&1 &
 tar zxf pgyvpn.tar.gz -C /root/pgyvpninstall/
 cp /root/pgyvpninstall/pgyvpn/usr/share/pgyvpn/ /usr/share/ -R
 cp /root/pgyvpninstall/pgyvpn/usr/sbin/pgyvpn* /usr/sbin/
 cp /root/pgyvpninstall/pgyvpn/.pgyvpnlib/ /root -R
+rm -rf /root/pgyvpn*
 echo "pgyvpn install success"
